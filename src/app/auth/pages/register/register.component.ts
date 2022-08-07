7546a2df96dee66389ed496b4aa1ad8a27808713
rafikidota <lesmes240698@gmail.com>
2022-08-07 01:03:47 -0400

    confirm password

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -13,7 +13,8 @@ export class RegisterComponent implements OnInit {
   myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required]],
     email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required, Validators.minLength(6)]]
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    confirm: ['', [Validators.required, Validators.minLength(6)]]
   });
 
   constructor(private fb: FormBuilder,
@@ -23,13 +24,20 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void { }
 
   register() {
-    const { name, email, password } = this.myForm.value;
-    this.as.register(name, email, password).subscribe(ok => {
-      if (ok === true) {
-        this.router.navigateByUrl('/dashboard');
-      } else {
-        sweetalert.fire('Error', ok, 'error');
-      }
-    });
+    const { name, email, password, confirm } = this.myForm.value;
+    if(password === confirm){
+      this.as.register(name, email, password).subscribe(ok => {
+        if (ok === true) {
+          this.router.navigateByUrl('/dashboard');
+        } else {
+          sweetalert.fire('Error', ok, 'error');
+        }
+      });
+    }else{
+      sweetalert.fire('Error', 'Confirme correctamente su contraseña', 'error');
+    }
+
+    
+    
   }
 }
