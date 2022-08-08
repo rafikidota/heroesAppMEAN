04a75cbbe1f75b73933e5b27beb98185ca70b6eb
rafikidota <lesmes240698@gmail.com>
2022-08-08 03:39:19 -0400

    Update reset-password.component.ts

--- src/app/auth/pages/reset-password/reset-password.component.ts
+++ src/app/auth/pages/reset-password/reset-password.component.ts
@@ -1,14 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import sweetalert from 'sweetalert2';
 
 @Component({
   selector: 'app-reset-password',
   templateUrl: './reset-password.component.html'
 })
 export class ResetPasswordComponent implements OnInit {
+  myForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    confirm: ['', [Validators.required, Validators.minLength(6)]]
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder,
+    private router: Router,
+    private as: AuthService) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  save() {
+    const { email, password, confirm } = this.myForm.value;
+    if (password === confirm) {
+      this.as.resetPassword(email, password).subscribe(ok => {
+
+        if (ok === true) {
+          this.router.navigateByUrl('/heroes');
+        } else {
+          sweetalert.fire('Error', ok, 'error');
+        }
+      });
+    }
+
   }
-
 }
